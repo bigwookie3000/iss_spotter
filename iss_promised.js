ebe440f4ecd5af7e13aec7affa0a99926f544cc7
Ryan Williams <bigwookie3000@gmail.com>
2020-09-25 04:20:58 +0000

    promised

---
+++ iss_promised.js
@@ -0,0 +1,39 @@
+const request = require('request-promise-native');
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function(body) {
+  return request('https://api.ipify.org?format=json');
+   
+  };
+  const fetchCoordsByIP = function(body) {
+    const ip = JSON.parse(body).ip;
+    return request(`https://ipvigilante.com/json/${ip}`);
+  };
+  /*
+ * Requests data from api.open-notify.org using provided lat/long data
+ * Input: JSON body containing geo data response from ipvigilante.com
+ * Returns: Promise of request for fly over data, returned as JSON string
+ */
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body).data;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  return request(url);
+};
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation };
